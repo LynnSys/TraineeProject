19e78137424079555a022d9a333893f3da8e2140
Lynn Locksley <160457651+LynnSys@users.noreply.github.com>
2024-09-24 15:40:26 +0530

    start

---
+++ TraineeApplication.Models/Models/Villain.cs
@@ -0,0 +1,10 @@
+﻿namespace TraineeApplication.Models.Models
+{
+    public class Villain
+    {
+        public int Id { get; set; }
+        public string VillianName { get; set; }
+        public string Superpower { get; set; }
+        public string EvilPlan { get; set; }
+    }
+}
---
+++ TraineeApplication.Services/Services/VillainService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraineeApplication.Models.Models;
+using TraineeApplication.Services.IServices;
+
+namespace TraineeApplication.Services.Services
+{
+	public class VillainService : IVillainService
+	{
+		public Villain GetVillanById(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
